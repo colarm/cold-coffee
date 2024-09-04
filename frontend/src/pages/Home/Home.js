c68d71e6164f2df4b7e84b0b2fbfd728d0162a2e
colarm <haichzh@gmail.com>
2024-09-05 03:10:50 +0800

    fix

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -3,14 +3,14 @@ import { useState } from 'react';
 
 // Language pack
 let lang = navigator.language || '';
-lang = 'zh-TW'; // 测试用，实际使用时应根据用户浏览器语言设置
+// lang = 'zh-TW'; // 测试用，实际使用时应根据用户浏览器语言设置
 const textList = {
   'en-GB': {
-    title: 'COFFEE',
+    title: 'COLD COFFEE',
     search: 'search',
   },
   'en-US': {
-    title: 'COFFEE',
+    title: 'COLD COFFEE',
     search: 'search',
   },
   'zh-CN': {
@@ -37,8 +37,7 @@ const searchEngineList = {
 const searchEngine = 'Google';
 
 function search(inputValue) {
-  window.location.href =
-    searchEngineList[searchEngine] + encodeURIComponent(inputValue);
+  window.open(searchEngineList[searchEngine] + encodeURIComponent(inputValue));
 }
 
 function onKeyUp(event, inputValue) {
@@ -52,15 +51,16 @@ function Home() {
 
   return (
     <div className="home">
-      <h1>{text[lang].title}</h1>
+      <h1 className='title'>{text[lang].title}</h1>
       <div>
         <input
+          className='search'
           type="text"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
           onKeyUp={(e) => onKeyUp(e, inputValue)}
         />
-        <button onClick={() => search(inputValue)}>{text[lang].search}</button>
+        <button className='searchButton' onClick={() => search(inputValue)}>{text[lang].search}</button>
       </div>
     </div>
   );
