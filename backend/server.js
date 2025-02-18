74ebd07a24634b0c8146120b1ce3b033b984f0ec
Colarm <haichzh@gmail>
2025-02-18 19:42:01 +0000

    Added https certificate

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
+const https = require('https');
+const http = require("http");
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 443;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-// 静态文件服务
+// 读取 Let’s Encrypt 证书
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/coldcoffee.cc/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/coldcoffee.cc/fullchain.pem"),
+};
 
+// 静态文件服务
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
@@ -16,6 +24,14 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+// 创建 HTTP 服务器并重定向到 HTTPS
+http.createServer((req, res) => {
+  res.writeHead(301, { Location: "https://" + req.headers.host + req.url });
+  res.end();
+}).listen(80, () => {
+  console.log("Redirecting HTTP to HTTPS...");
+});
+
+https.createServer(options, app).listen(port, () => {
+  console.log("HTTPS Server running on port " + port);
 });
