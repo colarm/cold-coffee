1134d91748ac6a0ddf8fb0fa1b1956f095b1595d
Colarm <haichzh@gmail.com>
2025-03-26 12:50:20 +0000

    remove https cert

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,21 @@
-const https = require('https');
-const http = require("http");
-const fs = require('fs');
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const port = process.env.PORT || 443;
+const port = process.env.PORT || 80;
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-// 读取 Let’s Encrypt 证书
-const options = {
-  key: fs.readFileSync("/etc/letsencrypt/live/coldcoffee.cc/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/coldcoffee.cc/fullchain.pem"),
-};
+app.use(express.static(path.join(__dirname, "build")));
 
 // 静态文件服务
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // 通配符路由
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-// 创建 HTTP 服务器并重定向到 HTTPS
-http.createServer((req, res) => {
-  res.writeHead(301, { Location: "https://" + req.headers.host + req.url });
-  res.end();
-}).listen(80, () => {
-  console.log("Redirecting HTTP to HTTPS...");
-});
-
-https.createServer(options, app).listen(port, () => {
-  console.log("HTTPS Server running on port " + port);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
